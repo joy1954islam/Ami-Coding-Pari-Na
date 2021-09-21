6b74830bbfa4166fae9c12f805e56e94fcb6362d
joy1954islam <joyislam1954@gmail.com>
2021-09-21 22:11:28 +0600

    create a models name khoj and migrate

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Khoj(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    input_values = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.input_values)
