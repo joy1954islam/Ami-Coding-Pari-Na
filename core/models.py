34fa0d7ec7d8d59ea4f0db7fdfc8f60dd065124e
joy1954islam <joyislam1954@gmail.com>
2021-09-22 22:08:11 +0600

    models filed modify

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Khoj(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     input_values = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField()
 
     def __str__(self):
         return str(self.input_values)
